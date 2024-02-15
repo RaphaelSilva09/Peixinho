ec509f2dc6c79949720eba289fc74f1b65ab273e
Raphael Silva <raphael.silva@sou.inteli.edu.br>
2024-02-15 19:45:40 -0300

    finalVersion

--- script/peixe.js
+++ script/peixe.js
@@ -2,46 +2,50 @@ var config = {
     type: Phaser.AUTO,
     width: window.innerWidth,
     height: window.innerHeight,
-
+  
     scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-
-};
-
-var game = new Phaser.Game(config);
-var peixin;
-
-function preload() {
-    this.load.image('mar', 'assets/bg_azulescuro.png');
-    this.load.image('logo', 'assets/logointeli_branco.png');
-
+      preload: preload,
+      create: create,
+      update: update,
+    },
+  };
+  
+  var game = new Phaser.Game(config);
+  var peixin;
+  
+  function preload() {
+    // pré carregando a imagem do cenário e do logo do inteli
+    this.load.image("mar", "assets/bg_azulescuro.png");
+    this.load.image("logo", "assets/logointeli_branco.png");
+    this.load.image('crustaceo', 'assets/peixes/crustaceo.png')
+  
     // guardando o peixin numa variável
-    peixin = this.load.image('peixe', 'assets/peixes/peixe_serio.png');
-
-}
-
-function create() {
-    this.add.image(400, 300, 'mar').setScale(2.2);
-    this.add.image(1070, 80, 'logo').setScale(0.75);
-
-    peixin = this.add.image(400, 300, 'peixe').setOrigin(0.5, 0.5)
+    peixin = this.load.image("peixe", "assets/peixes/peixe_serio.png");
+  }
+  
+  function create() {
+    //adicionando as imagens à tela, do mar, logo e crustáceo
+    this.add.image(400, 300, "mar").setScale(2.2);
+    this.add.image(1070, 80, "logo").setScale(0.75);
+    this.add.image(800, 500, 'crustaceo');
+  
+    //adicionando o peixe à tela
+    peixin = this.add.image(400, 300, "peixe").setOrigin(0.5, 0.5);
     peixin.setFlip(true, false);
-}
-
-function update() {
-    
-    //
+  }
+  
+  function update() {
+      //configurando peixe virando para o lado que está nadando
     if (this.input.x > peixin.x) {
-        peixin.setScale(1).setFlip(true, false);
-        
+      peixin.setScale(1).setFlip(true, false);
     } else {
-        peixin.setScale(-1).setFlip(true, true);
-        
+      peixin.setFlip(false, false);
     }
-
+  
+    //input de movimentação do peixe seguindo o mouse
     peixin.x = this.input.x;
     peixin.y = this.input.y;
-}
+  }
+  
+  //resposta da pergunta da atividade
+  console.log("Desde criança já programei, porém sempre coisas simples e sem entender a ideia por trás... fico muito feliz que isso esteja mudando a partir de hoje")
--- script/respota.js
+++
@@ -1,2 +0,0 @@
-var resposta = "Desde criança já programei, porém sempre coisas simples e sem entender a ideia por trás... fico muito feliz que isso esteja mudando a partir de hoje";
-    console.log(resposta);
